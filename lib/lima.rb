60f51971f5b387cf4044ee58699513686add1319
Sachin Singh <sachin.singh@metacube.com>
2016-10-22 00:25:50 +0530

    make lima use rack

--- lib/lima.rb
+++ lib/lima.rb
@@ -1,5 +1,9 @@
 require "lima/version"
 
 module Lima
-  # Your code goes here...
+  class Application
+    def call(env)
+    	[200, {'Content-Type' => 'text/html'}, ['Hello from Ruby on Lima!']]
+    end	
+  end	
 end
